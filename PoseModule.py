74980c4be579d17c2a05fb1884538d4f58ecab36
CodeMon <162836258+howaqw@users.noreply.github.com>
2024-04-22 14:27:50 -0700

    Update PoseModule.py

--- PoseModule.py
+++ PoseModule.py
@@ -1,23 +1,24 @@
-import cv2
+import cv2  #import opencv
 import mediapipe as mp
 import time
-
+###############################################
 
 class PoseDetector:
 
-    def __init__(self, mode = False, upBody = False, smooth=True, detectionCon = 0.5, trackCon = 0.5):
+    def __init__(self, mode = False, upBody = False, smooth=True, detecCon = 0.5, trackCon = 0.5):
 
         self.mode = mode
         self.upBody = upBody
         self.smooth = smooth
-        self.detectionCon = detectionCon
+        self.detectionCon = detecCon
         self.trackCon = trackCon
 
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
         self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, self.detectionCon, self.trackCon)
 
-    def findPose(self, img, draw=True):
+    def findPose(self, img, draw=True):  
+        #Find Pose Function
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.pose.process(imgRGB)
         #print(results.pose_landmarks)
